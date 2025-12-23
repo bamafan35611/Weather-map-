0466d8c87549f52d99a9e342f74ba09943ce4ef1
bamafan35611 <bamafan35611@gmail.com>
2025-12-23 15:45:42 -0600

    Add files via upload

--- aprs_integration.py
+++ aprs_integration.py
@@ -241,15 +241,15 @@ class APRSIntegration:
             
             # Check for significant conditions
             
-            # High winds
-            wind_gust = weather.get('wind_gust', 0)
-            if wind_gust >= 60:  # 60+ mph gusts (severe threshold - filters bad home station data)
-                significant.append({
-                    'type': 'wind',
-                    'callsign': callsign,
-                    'value': wind_gust,
-                    'timestamp': report['timestamp']
-                })
+            # High winds - DISABLED (too many false reports from home weather stations)
+            # wind_gust = weather.get('wind_gust', 0)
+            # if wind_gust >= 60:  # 60+ mph gusts (severe threshold - filters bad home station data)
+            #     significant.append({
+            #         'type': 'wind',
+            #         'callsign': callsign,
+            #         'value': wind_gust,
+            #         'timestamp': report['timestamp']
+            #     })
             
             # Heavy rainfall
             rain_1h = weather.get('rain_1h', 0)
